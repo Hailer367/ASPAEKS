85caea65b64654f785d8503d7fc01ade1a85d1e3
Hailer367 <brookwondwossen442@gmail.com>
2025-07-12 07:07:17 -0700

    Create index.js

---
+++ runeflip/src/pages/index.js
@@ -0,0 +1,18 @@
+import UpperPanel from '../components/UpperPanel';
+import LowerPanel from '../components/LowerPanel';
+
+export default function RuneFlip() {
+  return (
+    <div className="flex flex-col h-screen">
+      {/* Upper Half (60vh) - Interactive Coin Zone */}
+      <div className="flex-1" style={{ height: '60vh' }}>
+        <UpperPanel />
+      </div>
+      
+      {/* Lower Half (40vh) - Info Panels */}
+      <div className="flex-1" style={{ height: '40vh' }}>
+        <LowerPanel />
+      </div>
+    </div>
+  );
+}
