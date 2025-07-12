2ece9a0bf6361c20d713269d21ce88089d631bbb
Hailer367 <brookwondwossen442@gmail.com>
2025-07-12 08:28:14 -0700

    Update _app.js

--- runeflip/src/pages/_app.js
+++ runeflip/src/pages/_app.js
@@ -1,16 +1,8 @@
-// Import global CSS (required)
+// Remove font imports if not installed
 import '../styles/globals.css';
 
-// Optional: Import fonts if not using globals.css
-import '@fontsource/rajdhani/400.css';
-import '@fontsource/rajdhani/700.css';
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="bg-[#0C0D12] text-white font-sans min-h-screen">
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
