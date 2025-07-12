35d0040a9dcb1989f9df9f8fabcd160638bb9a8c
Hailer367 <brookwondwossen442@gmail.com>
2025-07-12 04:52:54 -0700

    Update WalletConnection.js

--- runeflip_v2/runeflip/components/WalletConnection.js
+++ runeflip_v2/runeflip/components/WalletConnection.js
@@ -1,1 +1,21 @@
-// Wallet Connection Logic
+import { useWallet, WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react';
+import { WalletModalContextProvider } from '@solana/wallet-adapter-react-ui';
+
+export default function WalletConnection() {
+  const wallet = useWallet();
+
+  return (
+    <div className="absolute top-4 right-4">
+      <WalletModalProvider>
+        <WalletModalContextProvider>
+          <WalletMultiButton />
+        </WalletModalContextProvider>
+      </WalletModalProvider>
+      {wallet.connected && (
+        <p className="text-xs mt-1 truncate w-48 text-right">
+          {wallet.publicKey?.toString()}
+        </p>
+      )}
+    </div>
+  );
+}// Wallet Connection Logic
