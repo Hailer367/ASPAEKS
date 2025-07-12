8692c04202f0e059098df581c639ed81e7228ade
Hailer367 <brookwondwossen442@gmail.com>
2025-07-12 05:32:03 -0700

    Update tailwind.config.js

--- runeflip_v2/runeflip/tailwind.config.js
+++ runeflip_v2/runeflip/tailwind.config.js
@@ -1,1 +1,18 @@
-// Tailwind CSS Config
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3FE0D0',
+        secondary: '#9F6CFF',
+        gold: '#FFD66B',
+        dark: '#0C0D12',
+        card: '#1A1B22',
+      },
+    },
+  },
+  plugins: [],
+}// Tailwind CSS Config
