954ea5b10672d0a2b37054da6147be64523aa02b
Hailer367 <brookwondwossen442@gmail.com>
2025-07-12 05:01:46 -0700

    Update _app.js

--- runeflip_v2/runeflip/pages/_app.js
+++ runeflip_v2/runeflip/pages/_app.js
@@ -1,1 +1,16 @@
-// Custom App Wrapper
+import '../styles/globals.css';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { clusterApiUrl } from '@solana/web3.js';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ConnectionProvider endpoint={process.env.NEXT_PUBLIC_SOLANA_RPC_URL || clusterApiUrl('devnet')}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}// Custom App Wrapper
