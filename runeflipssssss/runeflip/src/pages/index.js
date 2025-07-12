590e8b80fd61469eee9b051881713124118ab321
Hailer367 <brookwondwossen442@gmail.com>
2025-07-12 04:27:00 -0700

    Update index.js

--- runeflipssssss/runeflip/src/pages/index.js
+++ runeflipssssss/runeflip/src/pages/index.js
@@ -1,8 +1,16 @@
+// src/pages/index.js
 export default function Home() {
   return (
-    <div style={{ background: 'black', color: 'white', height: '100vh' }}>
-      <h1>RuneFlip</h1>
-      <p>Solana Coin Flip Game</p>
+    <div style={{
+      background: '#0C0D12',
+      height: '100vh',
+      color: 'white',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>RuneFlip is Loading...</h1>
     </div>
   );
 }
