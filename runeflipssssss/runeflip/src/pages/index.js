26fbd07c72f0a4674a8648813fce77b3fede90bb
Hailer367 <brookwondwossen442@gmail.com>
2025-07-12 03:21:28 -0700

    Update index.js

--- runeflipssssss/runeflip/src/pages/index.js
+++ runeflipssssss/runeflip/src/pages/index.js
@@ -1,1 +1,18 @@
-// Main Game UI
+import UpperPanel from '../components/UpperPanel';
+import LowerPanel from '../components/LowerPanel';
+
+export default function RuneFlip() {
+  return (
+    <div className="h-screen flex flex-col">
+      {/* Upper Half - Interactive Zone (60% height) */}
+      <div className="flex-1" style={{ height: '60vh' }}>
+        <UpperPanel />
+      </div>
+
+      {/* Lower Half - Information Panels (40% height) */}
+      <div className="flex-1" style={{ height: '40vh' }}>
+        <LowerPanel />
+      </div>
+    </div>
+  );
+}// Main Game UI
